fb0c6dd612ef8979108f8d31edfe50bc5e0e5399
albertolunia <albertolunia@gmail.com>
2024-01-15 11:43:41 -0300

    Seção4 - Aula 15

--- CursoDeAngular/src/app/app.component.ts
+++ CursoDeAngular/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NewComponent],
   template: `
   <!-- <router-outlet></router-outlet> -->
-  <h1>Curso de Angular</h1>
+  <app-new-component/>
   <h2>Components</h2>
   `,
 })
